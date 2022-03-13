95118b68f1329078cd1b91c133d874ef10b4873b
RomanVinichenko <chicagezzz@gmail.com>
2022-03-13 19:19:08 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-window.onload = function() {
-    window.addEventListener('scroll', function() {
-        let move = document.querySelector('.scroll');
-        if (window.pageYOffset > 1000) {
-            move.classList.add('scroll--active');
-        } else {
-            move.classList.remove('scroll--active');
-        }
-    })
-};
-
 // const control = document.querySelector('.home-top__slider');
 //     control.addEventListener('click', function () {
 //         this.classList.toggle('home-top__slider--active');
