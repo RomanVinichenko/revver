7d1c6deb8567c0cbb4b8f5ccd3ead14f10b4c494
RomanVinichenko <chicagezzz@gmail.com>
2022-03-17 12:14:24 +0200

    editing news ++

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
-// const control = document.querySelector('.home-top__slider');
-//     control.addEventListener('click', function () {
-//         this.classList.toggle('home-top__slider--active');
-// });
-//
-// const link = document.querySelector('.links');
-// const bunch = document.querySelector('.bunch');
-//     link.addEventListener('click', function () {
-//         bunch.classList.toggle('bunch--active');
-// });
+const control = document.querySelector('.home-top__slider');
+    control.addEventListener('click', function () {
+        this.classList.toggle('home-top__slider--active');
+});
+
+const link = document.querySelector('.links');
+const bunch = document.querySelector('.bunch');
+    link.addEventListener('click', function () {
+        bunch.classList.toggle('bunch--active');
+});
 
 $(function () {
     $('.question').slick({
