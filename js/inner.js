0a7cb8243deda054ee0e8d7563680ab34abf0aac
RomanVinichenko <chicagezzz@gmail.com>
2022-03-27 22:36:43 +0300

    Updates

--- js/inner.js
+++ js/inner.js
@@ -2,7 +2,7 @@ $(function () {
     $('.propose__info a').on('click', function (event) {
         event.preventDefault();
         var id = $(this).attr('href'),
-            top = $(id).offset().top - 105;
+            top = $(id).offset().top - 20;
         $('body,html').animate(
             {
                 scrollTop: top,
