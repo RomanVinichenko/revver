6712252d6d8e39303e17ad4caa6660dc9bc5bf97
RomanVinichenko <chicagezzz@gmail.com>
2022-03-28 13:01:12 +0300

    PopUp join

---
+++ app/js/join.js
@@ -0,0 +1,17 @@
+const buttonVacancy = document.querySelectorAll('.green-link')
+const popUp = document.querySelector('.popup')
+const close = document.querySelector('.popup__close')
+const body = document.body
+
+const sale = document.querySelectorAll('.sales__item');
+buttonVacancy.forEach((buttonVacancy) => {
+    buttonVacancy.addEventListener('click', function () {
+        popUp.classList.add('popup__active');
+            body.style.overflow = 'hidden';
+    });
+});
+
+close.addEventListener('click', function () {
+    popUp.classList.remove('popup__active');
+    body.style.overflow = 'auto';
+})
