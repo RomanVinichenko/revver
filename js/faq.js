9eb427c8e6642e2971581d2a55a62e7fda8a0268
RomanVinichenko <chicagezzz@gmail.com>
2022-04-01 00:43:54 +0300

    Updates

--- js/faq.js
+++
@@ -1,6 +0,0 @@
-const sale = document.querySelectorAll('.sales__item');
-sale.forEach((item) => {
-    item.addEventListener('click', function () {
-        this.classList.toggle('sales__item--active');
-    });
-});
