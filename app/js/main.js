d50fc9d386bf69e99138b95d44ed6156af7de896
RomanVinichenko <chicagezzz@gmail.com>
2022-04-13 15:34:03 +0300

    bugfix

--- app/js/main.js
+++ app/js/main.js
@@ -218,7 +218,7 @@ function newAnimOut(x) {
 }
 
 
-let elements = document.querySelectorAll('.job__wave');
+let elements = document.querySelectorAll('.move-text');
 
 elements.forEach(element => {
     let innerText = element.innerText;
