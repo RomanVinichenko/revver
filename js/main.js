815fdc944936062f3c81dabc769a3a170fd397ca
RomanVinichenko <chicagezzz@gmail.com>
2022-05-05 16:24:19 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -29,6 +29,27 @@ menuButton.addEventListener('click', function () {
     }
 });
 
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".interface__box").forEach(section => {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: section,
+            start: "center center",
+            // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+            end: () => "+=" + section.offsetWidth,
+            scrub: true,
+            pin: true,
+            anticipatePin: 1
+        },
+        defaults: {ease: "none"}
+    });
+    // animate the container one way...
+    tl.fromTo(section.querySelector(".afterImage"), { xPercent: 100, x: 0}, {xPercent: 0})
+        // ...and the image the opposite way (at the same time)
+        .fromTo(section.querySelector(".afterImage img"), {xPercent: 49, x: 0}, {xPercent: 0}, 0);
+});
+
 // PRODUCT POLZUN MOVING
 
 function MoveBarItem(x) {
