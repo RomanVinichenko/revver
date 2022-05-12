b473ae49b2d1f52a0dca884afa776cdea9d0f0f1
RomanVinichenko <chicagezzz@gmail.com>
2022-05-12 20:38:05 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -29,27 +29,6 @@ menuButton.addEventListener('click', function () {
     }
 });
 
-// GSAP
-gsap.registerPlugin(ScrollTrigger);
-gsap.utils.toArray(".interface__box").forEach(section => {
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: section,
-            start: "center center",
-            // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
-            end: () => "+=" + section.offsetWidth,
-            scrub: true,
-            pin: true,
-            anticipatePin: 1
-        },
-        defaults: {ease: "none"}
-    });
-    // animate the container one way...
-    tl.fromTo(section.querySelector(".afterImage"), { xPercent: 100, x: 0}, {xPercent: 0})
-        // ...and the image the opposite way (at the same time)
-        .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
-});
-
 // PRODUCT POLZUN MOVING
 
 function MoveBarItem(x) {
@@ -277,6 +256,27 @@ function imgIn() {
 function imgOut() {
     document.querySelector('.particle__img').classList.remove('particle__img--active');
 }
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".interface__box").forEach(section => {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: section,
+            start: "center center",
+            // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+            end: () => "+=" + section.offsetWidth,
+            scrub: true,
+            pin: true,
+            anticipatePin: 1
+        },
+        defaults: {ease: "none"}
+    });
+    // animate the container one way...
+    tl.fromTo(section.querySelector(".afterImage"), { xPercent: 100, x: 0}, {xPercent: 0})
+        // ...and the image the opposite way (at the same time)
+        .fromTo(section.querySelector(".afterImage img"), {xPercent: -100, x: 0}, {xPercent: 0}, 0);
+});
 
 // let testBlock = document.getElementById('test-block');
 // var currenrScrolPoint = 0;
